94ec84878ee8903de990ac919a52c2045da9f5a4
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-13 20:05:38 +0300

    add routes to companies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('companies', CompanyController::class)->middleware('auth');
