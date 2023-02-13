59f8fee5dc8360b453a3c7b2bc91cda6d993ade7
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-13 20:12:10 +0300

    edit table headers in companies index view

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Список компаний</h2>
+            </div>
+            <div class="pull-right mb-2">
+                <a class="btn btn-success" href="{{ route('companies.create') }}"> Создать</a>
+            </div>
+        </div>
+    </div>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>№</th>
+                <th>Название</th>
+                <th>Логотип</th>
+                <th>Email</th>
+                <th>Адрес</th>
+                <th width="280px">Действия</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($companies as $company)
+                <tr>
+                    <td>{{ $company->id }}</td>
+                    <td>{{ $company->logo }}</td>
+                    <td>{{ $company->name }}</td>
+                    <td>{{ $company->email }}</td>
+                    <td>{{ $company->address }}</td>
+                    <td>
+                        <form action="{{ route('companies.destroy',$company->id) }}" method="Post">
+                            <a class="btn btn-primary" href="{{ route('companies.edit',$company->id) }}">Edit</a>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+        </tbody>
+    </table>
+    {!! $companies->links() !!}
+</div>
+@endsection
