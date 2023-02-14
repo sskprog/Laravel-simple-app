ff27e0c4d5e6b2a70f06ca119aa1650c677c314c
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-14 22:50:41 +0300

    add validation rules in CompanyController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -8,96 +8,107 @@ use Illuminate\Http\Request;
 class CompanyController extends Controller
 {
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $companies = Company::orderBy('id','desc')->paginate(5);
+        $companies = Company::orderBy('id', 'desc')->paginate(5);
         return view('companies.index', compact('companies'));
     }
 
     /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('companies.create');
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'address' => 'required',
-        ]);
+        $this->validator($request);
+        // $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email|unique:companies,email',
+        //     'address' => 'required',
+        // ]);
 
         Company::create($request->post());
 
-        return redirect()->route('companies.index')->with('success','Company has been created successfully.');
+        return redirect()->route('companies.index')->with('success', 'Company has been created successfully.');
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  \App\company  $company
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  \App\company  $company
+     * @return \Illuminate\Http\Response
+     */
     public function show(Company $company)
     {
-        return view('companies.show',compact('company'));
+        return view('companies.show', compact('company'));
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  \App\Company  $company
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
     public function edit(Company $company)
     {
-        return view('companies.edit',compact('company'));
+        return view('companies.edit', compact('company'));
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \App\company  $company
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\company  $company
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, Company $company)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'address' => 'required',
-        ]);
+        // $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email|unique:companies,email',
+        //     'address' => 'required',
+        // ]);
+        $this->validator($request);
 
         $company->fill($request->post())->save();
 
-        return redirect()->route('companies.index')->with('success','Company Has Been updated successfully');
+        return redirect()->route('companies.index')->with('success', 'Company Has Been updated successfully');
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\Company  $company
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Company $company)
     {
         $company->delete();
-        return redirect()->route('companies.index')->with('success','Company has been deleted successfully');
+        return redirect()->route('companies.index')->with('success', 'Company has been deleted successfully');
+    }
+    protected function validator($request) {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:companies,email',
+            'address' => 'required',
+        ]);
+
     }
 }
