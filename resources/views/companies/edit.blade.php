ea58ec350f6e723e1d40592f4e8927ce46efd9e6
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-14 22:43:04 +0300

    add styles to companies/edit.blade.php

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -1,7 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-2">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-start mb-2">
+                <h2>Редактировать</h2>
+            </div>
+            <div class="float-end">
+                <a class="btn btn-primary" href="{{ route('companies.index') }}"> Назад</a>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-12 col-lg-6 ">
+            @if(session('status'))
+            <div class="alert alert-success mb-1 mt-1">
+                {{ session('status') }}
+            </div>
+            @endif
+            <form action="{{ route('companies.update',$company->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label for="name" class="form-label">Название</label>
+                    <input type="text" class="form-control" name="name" placeholder="Название компании"
+                        value="{{ $company->name }}">
+                    @error('name')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Электронная почта</label>
+                    <input type="text" class="form-control" name="email" placeholder="Электронная почта"
+                        value="{{ $company->email }}">
+                    @error('email')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Адрес</label>
+                    <input type="text" class="form-control" name="address" placeholder="Адрес компании"
+                        value="{{ $company->address }}">
+                    @error('address')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="input-group mb-3">
+                    <input type="file" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Сохранить</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+{{-- <div class="container mt-2">
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -55,5 +109,5 @@
             <button type="submit" class="btn btn-primary ml-3">Submit</button>
         </div>
     </form>
-</div>
+</div> --}}
 @endsection
