db2489ade27115be5e77a003aa309d4a9f375455
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-14 22:24:33 +0300

    add styles to companies/create.blade.php

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -1,54 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-2">
+
+<div class="container">
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left mb-2">
+            <div class="float-start mb-2">
                 <h2>Создать</h2>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('companies.index') }}"> Back</a>
+            <div class="float-end">
+                <a class="btn btn-primary" href="{{ route('companies.index') }}"> Назад</a>
             </div>
         </div>
     </div>
-    @if(session('status'))
-    <div class="alert alert-success mb-1 mt-1">
-        {{ session('status') }}
-    </div>
-    @endif
-    <form action="{{ route('companies.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Company Name:</strong>
-                    <input type="text" name="name" class="form-control" placeholder="Company Name">
-                    @error('name')
-                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-12 col-lg-6 ">
+            @if(session('status'))
+            <div class="alert alert-success mb-1 mt-1">
+                {{ session('status') }}
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Company Email:</strong>
-                    <input type="email" name="email" class="form-control" placeholder="Company Email">
-                    @error('email')
-                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Company Address:</strong>
-                    <input type="text" name="address" class="form-control" placeholder="Company Address">
-                    @error('address')
-                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            @endif
+            <form action="{{ route('companies.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @include('companies._form')
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 @endsection
