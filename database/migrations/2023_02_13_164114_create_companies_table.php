6ece7c803b8e37e32ccaec81d25513a2573f1dc1
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-21 21:48:39 +0300

    set logo properties nullable

--- database/migrations/2023_02_13_164114_create_companies_table.php
+++ database/migrations/2023_02_13_164114_create_companies_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('address');
-            $table->string('logo');
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
