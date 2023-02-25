cdb8cae0ecf8e1794395f1efd939936726079ba2
Stas Kushnir <stas.kushnir@gmail.com>
2023-02-25 22:20:31 +0300

    rename field in migration create_employees_table

--- database/migrations/2023_02_21_182912_create_employees_table.php
+++ database/migrations/2023_02_21_182912_create_employees_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained()->onDelete('cascade');
-            $table->string('full_name');
+            $table->string('emp_name');
             $table->string('email');
             $table->string('phone');
             $table->timestamps();
